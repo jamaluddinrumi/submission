69144b77a67fcf3865af8bf44bb711e676155b9a
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-05-12 03:50:06 +0000

    pwa: add a missing web manifest

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,11 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/images'),
           to: path.resolve(__dirname, 'dist/images')
+        },
+
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/')
         }
       ]
     }),
