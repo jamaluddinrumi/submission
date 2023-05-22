4cd612f1876f6adeea4c6f0e038c2900dca01d99
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-05-22 10:00:24 +0000

    chore: set chrome headless as browser for karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,5 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 // Karma configuration
 // Generated on Fri Jul 03 2020 20:15:52 GMT+0700 (Western Indonesia Time)
 module.exports = function (config) {
@@ -68,7 +70,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
