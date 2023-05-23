9ab489eb60c289d238d1450afb70fa60a2133c0c
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-05-23 17:34:00 +0000

    chore: add webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default
 const ImageminMozjpeg = require('imagemin-mozjpeg')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   experiments: {
@@ -65,6 +66,7 @@ module.exports = {
 
         })
       ]
-    })
+    }),
+    new BundleAnalyzerPlugin()
   ]
 }
