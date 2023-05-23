ff3305d84d1e1856daa437dcf6f4fb5df5a13b44
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-05-23 15:54:36 +0000

    perf: compress jpeg

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default
+const ImageminMozjpeg = require('imagemin-mozjpeg')
 
 module.exports = {
   experiments: {
@@ -56,6 +58,14 @@ module.exports = {
     }),
     new FaviconsWebpackPlugin({
       logo: path.resolve(__dirname, 'src/images/favicon-512x512.png')
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true
+        })
+      ]
     })
   ]
 }
