58d0e83286205ed0c9cbd751ba8681d850f8a7e7
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-05-24 14:30:56 +0700

    chore: delete unused consts

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import swRegister from './utils/sw-register'
 import 'lazysizes'
 import 'lazysizes/plugins/parent-fit/ls.parent-fit'
 
-const START = 10
-const NUMBER_OF_IMAGES = 100
-
 const app = new App({
   content: document.querySelector('#main-content > .content')
 })
