6e4c6a61025310966b440f4c32ec2d20b451c030
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-05-25 09:16:35 +0000

    perf: don't use lazysizes for hero images in sake for good LCP

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -39,9 +39,20 @@ const Detail = {
     const restaurantElement = document.querySelector('#restaurant')
     restaurantElement.innerHTML = `
     <picture>
-      <source data-srcset="https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}" media="(max-width: 415px)" />
-      <source data-srcset="https://restaurant-api.dicoding.dev/images/medium/${restaurant.pictureId}" media="(max-width: 1024px)" />
-      <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}" alt="${restaurant.name}" class="lazyload w-full" crossorigin="anonymous" />
+      <source 
+        srcset="https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}" 
+        media="(max-width: 415px)" 
+      />
+      <source 
+        srcset="https://restaurant-api.dicoding.dev/images/medium/${restaurant.pictureId}" 
+        media="(max-width: 1024px)" 
+      />
+      <img 
+        src="https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}" 
+        alt="${restaurant.name}" 
+        class="w-full" 
+        crossorigin="anonymous" 
+      />
     </picture>
       <div class="card">
         <div class="rating">
